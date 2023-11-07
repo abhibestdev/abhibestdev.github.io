6af9f6358e7f6646136ac4f874eedd00604489cb
chirpthetwerp <32555127+abhibestdev@users.noreply.github.com>
2023-11-07 11:18:20 -0500

    client

---
+++ itis3135/client/scripts/default.js
@@ -0,0 +1,12 @@
+function loadHeader() {
+    document.getElementById("default-header").innerHTML = 
+    "<ul>" +
+    "<li><a href='index.html'>Home</a></li>" + 
+    "<li><a href='listings.html'>Listings</a></li>" +
+    "<li><a href='testimonials.html'>Testimonials</a></li>" +
+    "<li class='right'><a href='contact.html'>Contact</a></li>" +
+    "<li class='right'><a href='about.html'>About</a></li>" + 
+    "</ul>"
+}
+
+loadHeader();
