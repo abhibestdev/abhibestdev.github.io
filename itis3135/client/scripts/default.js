a90538fa7ecb2073a504d421e1cdcfa8051e1782
chirpthetwerp <32555127+abhibestdev@users.noreply.github.com>
2023-11-12 23:01:21 -0500

    client project

--- itis3135/client/scripts/default.js
+++ itis3135/client/scripts/default.js
@@ -1,12 +1,34 @@
 function loadHeader() {
-    document.getElementById("default-header").innerHTML = 
-    "<ul>" +
-    "<li><a href='index.html'>Home</a></li>" + 
-    "<li><a href='listings.html'>Listings</a></li>" +
-    "<li><a href='testimonials.html'>Testimonials</a></li>" +
-    "<li class='right'><a href='contact.html'>Contact</a></li>" +
-    "<li class='right'><a href='about.html'>About</a></li>" + 
-    "</ul>"
+    document
+        .getElementById("default-header")
+        .innerHTML = "<ul><li><a href='index.html'>Home</a></li><li><a href='listings.html'>Listings</" +
+            "a></li><li><a href='testimonials.html'>Testimonials</a></li><li class='right'><a" +
+            " href='contact.html'>Contact</a></li><li class='right'><a href='about.html'>Abou" +
+            "t</a></li></ul>"
 }
 
 loadHeader();
+
+//From w3schools for slide show
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
